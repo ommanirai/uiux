97292768455201df3a0b3128144f58453f830690
ommani <ommanirai27@gmail.com>
2023-12-03 08:09:32 +0545

    array and object

---
+++ js/array.js
@@ -0,0 +1,19 @@
+/*
+array
+=> item inside array is called elements of array
+=> index of element always start with 0
+=> js array is heterogenious
+
+
+accessign the elements of array
+syntax:
+array_name[index]
+
+*/
+var fruits = ['apple', 'mango', 'kiwi', true, 1, [1,2,3,4], {
+    name: 'ram',
+    address: 'Vedu'
+}]
+console.log(fruits)
+console.log(fruits[0],fruits[1])
+console.log(fruits[5])
---
+++ js/object.js
@@ -0,0 +1,52 @@
+// object
+// object is collection of key-value, property-value, name-value pair
+// block: {}
+var phone = {
+    // brand: 'mi',
+    brand: {
+        brand1: 'samsung',
+        brand2: 'apple',
+        brand3: 'redmi'
+    },
+    price: 13000,
+    ram: '6gb'
+}
+
+console.log(phone.brand.brand1)
+console.log(phone['brand'].brand1)
+
+// brand, price and ram => key/property/name
+// mi, 13000, 6gb => value
+// console.log(phone)
+
+/*
+two way of accessing object value
+1. dot notation
+syntax:
+object_name.key
+
+2. bracket notation
+syntax:
+object_name['key']
+
+
+*/
+// dot notation method
+// var brand = phone.brand;
+// console.log(brand)
+// console.log(phone.brand)
+
+// bracket notation method
+// var brnd = phone['brand']
+// console.log(brnd)
+
+/*
+adding property-value to object
+syntax:
+
+object_name.key = value
+
+*/
+phone.ram = '8gb'
+phone.status = 'available'
+console.log(phone)
