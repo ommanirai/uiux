c0a363586b20c3d46fbec48bb4d619728d9a0920
ommani <ommanirai27@gmail.com>
2023-12-07 08:06:07 +0545

    loop

---
+++ js/loop.js
@@ -0,0 +1,49 @@
+/*
+Loop
+types:
+1. for
+syntax:
+for(initialization; condition; increment/decrement){
+    body of for loop
+}
+
+2. while
+3. do while
+
+*/
+// print number from 10 to 1
+// initialization
+var i = 10;
+// condition
+// i>= 1
+
+// increment
+// i++
+var n = Number(prompt("enter number"))
+for (i = 1; i <= n; i++) {
+    var j = 0
+    // if(i%2==0){
+    // console.log(i)
+    j = j + i;
+    // }
+}
+// wap(write a program) to print the even number from 1 to 50
+// wap to print the sum of number from 1 to 10
+// wap to print sum of natural number
+// wap to print the factorial of a number 5
+
+// wap to check the prime number
+// wap to print the fibonacci series
+// 0 1
+// 1
+// 2
+// 3
+// 5
+// 8
+
+
+// j+=i;
+console.log(j)
+
+// 5!
+// 5 * 4 * 3 * 2 * 1
