8758e93ddd89fda6e3763e6490088887a6ad9636
ommani <ommanirai27@gmail.com>
2023-12-08 08:18:55 +0545

    for loop

--- js/loop.js
+++ js/loop.js
@@ -8,25 +8,47 @@ for(initialization; condition; increment/decrement){
 }
 
 2. while
+syntax:
+initializatin;
+while(condition){
+    body of while
+    increment/decrement
+}
+
+
 3. do while
+initialization
+do{
+    body of do while
+    increment/decrement
+}while(condition)
+
 
 */
-// print number from 10 to 1
+// print number from 1 to 10
+// declaration
+// var i;
 // initialization
-var i = 10;
+// i = 1;
 // condition
-// i>= 1
+// i <=10
+
+// x = i++
+// x = i
+// x = x + i
+
 
 // increment
 // i++
-var n = Number(prompt("enter number"))
-for (i = 1; i <= n; i++) {
-    var j = 0
-    // if(i%2==0){
-    // console.log(i)
-    j = j + i;
-    // }
-}
+// var n = Number(prompt("enter number"))
+// for (i = 1; i <= 10; i++) {
+// var j = 0
+// console.log(i)
+// if(i%2==0){
+// console.log(i)
+// j = j + i;
+// }
+// }
 // wap(write a program) to print the even number from 1 to 50
 // wap to print the sum of number from 1 to 10
 // wap to print sum of natural number
@@ -43,7 +65,39 @@ for (i = 1; i <= n; i++) {
 
 
 // j+=i;
-console.log(j)
+// console.log(j)
 
 // 5!
 // 5 * 4 * 3 * 2 * 1
+var n = 7
+var a = 0
+var b = 1
+// var c = a + b;
+// a = b;
+// b =c;
+// if(n==1){
+//     console.log(a)
+// }
+// else if(n==2){
+//     console.log(a)
+//     console.log(b)
+// }
+// else{
+//     console.log(0)
+//     console.log(1)
+//     for(i=1;i<=n-2;i++){
+
+//     }
+// }
+// while
+var j = 11
+while (j <= 10) {
+    console.log(j)
+    j++
+}
+console.log("-------*******--------")
+var i = 11
+do {
+    console.log(i)
+    i++
+} while (i <= 10)
