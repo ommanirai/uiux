ac2dadb917e66676b4048f071738da2a80ee253f
ommani <ommanirai27@gmail.com>
2023-12-17 08:01:33 +0545

    inbuilt methods

---
+++ js/inbuilt_method.js
@@ -0,0 +1,44 @@
+/*
+setTimeout()
+setInterval()
+typeof()
+Number()
+*/
+// string
+var str = "GoodMorning"
+var email = 'rag@gmail.com'
+// var result = str.length
+// // console.log(result)
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// console.log(str.substring(1,3))
+// console.log(str.substr(1,3))
+// console.log(email.includes('@gmail'))
+
+// split 
+// string to array
+var fruits = 'mango, apple, banana, kiwi, papaya'
+var fru = fruits.split('a')
+// console.log(fru)
+
+// number
+var number = 100.1268
+// console.log(number.toFixed(0))
+// console.log(!isNaN(number))
+
+// object
+var laptops = {
+    name: 'acer',
+    gen: 'i9',
+    ram: '8gb',
+    color: 'grey'
+}
+// console.log(laptops.hasOwnProperty('name'))
+// console.log(Object.keys(laptops))
+// console.log(Object.values(laptops))
+// for in
+// for(var item in laptops){
+//     console.log(item)
+//     console.log(laptops[item])
+// }
+
