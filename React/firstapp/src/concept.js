3b77b21f723651894ed19322809c87a890f9c5bc
ommani <ommanirai27@gmail.com>
2024-01-04 08:21:24 +0545

    react

---
+++ React/firstapp/src/concept.js
@@ -0,0 +1,28 @@
+/*
+component
+=> basic building block of react
+=> is used to create user interfaces
+
+=> compoent must return signle html node
+
+component can be written in two ways
+1. functional component(stateless component before 16.8)
+2. class component(statefull component before 16.8)
+
+// react => hook => functional component can have state
+
+component are of two types in context with data
+1. statefull component
+2. stateless component
+
+component data
+1. state => data within a component
+2. props => incoming data for a component
+
+
+
+
+
+
+
+*/
