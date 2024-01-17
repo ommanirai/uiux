1ca1c8365acecefc26a6bf4489f9dad6b403efef
ommani <ommanirai27@gmail.com>
2024-01-17 08:00:08 +0545

    register

--- React/firstapp/src/component/Auth/Register/Register.component.js
+++ React/firstapp/src/component/Auth/Register/Register.component.js
@@ -59,6 +59,47 @@ export class Register extends Component {
                     : 'required field'
                 break;
             case 'password':
+                errorMsg = this.state.data['confirmPassword']
+                    ? this.state.data['confirmPassword'] === this.state.data[fieldName]
+                        ? ""
+                        : ' password didnot match with confirm password'
+                    : this.state.data[fieldName]
+                        ? this.state.data[fieldName].match(/[a-z]/)
+                            ? this.state.data[fieldName].match(/[A-Z]/)
+                                ? this.state.data[fieldName].match(/[0-9]/)
+                                    ? this.state.data[fieldName].match(/[!@#$%^&*]/)
+                                        ? this.state.data[fieldName].length < 8
+                                            ? 'password must containt atleast 8 character'
+                                            : this.state.data[fieldName].length > 30
+                                                ? 'password must contain maximum 30 character'
+                                                : ''
+                                        : 'password must contain atleast one special character'
+                                    : 'password must contain atleast one number'
+                                : 'password must contain atleast one uppercase alphabet'
+                            : 'password must contain atleat one lowercase alphabet'
+                        : 'required filed'
+                break
+            case 'confirmPassword':
+                errorMsg = this.state.data['password']
+                    ? this.state.data['password'] === this.state.data[fieldName]
+                        ? ""
+                        : 'confirm password didnot match with password'
+                    : this.state.data[fieldName]
+                        ? this.state.data[fieldName].match(/[a-z]/)
+                            ? this.state.data[fieldName].match(/[A-Z]/)
+                                ? this.state.data[fieldName].match(/[0-9]/)
+                                    ? this.state.data[fieldName].match(/[!@#$%^&*]/)
+                                        ? this.state.data[fieldName].length < 8
+                                            ? 'confirm password must containt atleast 8 character'
+                                            : this.state.data[fieldName].length > 30
+                                                ? 'confirm password must contain maximum 30 character'
+                                                : ''
+                                        : 'confirm password must contain atleast one special character'
+                                    : 'confirm password must contain atleast one number'
+                                : 'confirm password must contain atleast one uppercase alphabet'
+                            : 'confirm password must contain atleat one lowercase alphabet'
+                        : 'required filed'
+                break;
 
         }
         this.setState(previousError => ({
@@ -109,9 +150,11 @@ export class Register extends Component {
 
                     <label className="fs-4">password</label>
                     <input type="text" name="password" className="form-control form-control-lg mb-3" onChange={this.handleChange} />
+                    <p className="text-danger">{this.state.error.password}</p>
 
                     <label className="fs-4">confirmPassword</label>
                     <input type="text" name="confirmPassword" className="form-control form-control-lg mb-3" onChange={this.handleChange} />
+                    <p className="text-danger">{this.state.error.confirmPassword}</p>
 
                     <label className="fs-4">address</label>
                     <input type="text" name="address" className="form-control form-control-lg mb-3" onChange={this.handleChange} />
@@ -128,7 +171,6 @@ export class Register extends Component {
                         isSubmitting={this.state.isSubmitting}
                         isValidForm={this.state.isValidForm}
                     ></Button>
-
                 </form>
             </div>
         )
